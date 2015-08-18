0de03afdf38efd0b3799346e7bede2677ce7bcb9
cristiingineru <cristi.ingineru@gmail.com>
2015-08-18 19:18:06 +0300

    Immutable package

---
+++ Chaining.Tests/Immutable.Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Immutable;
+
+namespace Chaining.Tests
+{
+    [TestClass]
+    public class ImmutableTests
+    {
+        [TestMethod]
+        public void ImmitableArray()
+        {
+            var array = ImmutableArray<int>.Empty;
+
+            var array2 = array.Add(10);
+
+            Assert.AreNotEqual(array, array2);
+        }
+
+        [TestMethod]
+        public void ImmutableDictionary()
+        {
+            var dictionary = ImmutableDictionary<int, string>.Empty;
+
+            var dictionary2 = dictionary.Add(1, "a");
+
+            Assert.AreNotEqual(dictionary, dictionary2);
+        }
+    }
+}
