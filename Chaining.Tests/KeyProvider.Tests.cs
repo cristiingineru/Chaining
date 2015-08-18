d04b31b82f9e746cc6dedf8006485779ffc6a17d
cristiingineru <cristi.ingineru@gmail.com>
2015-08-18 21:08:07 +0300

    KeyProvider

---
+++ Chaining.Tests/KeyProvider.Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chaining.Tests
+{
+    [TestClass]
+    public class KeyProviderTests
+    {
+        [TestMethod]
+        public void KeyProvider_Key_ReturnsKey()
+        {
+            var provider = new KeyProvider();
+
+            var key = provider.Key();
+        }
+
+        [TestMethod]
+        public void KeyProvider_CallingKeyTwice_ReturnsNewKeyEachTime()
+        {
+            var provider = new KeyProvider();
+
+            var key1 = provider.Key();
+            var key2 = provider.Key();
+
+            Assert.AreNotEqual(key1, key2);
+        }
+    }
+}
---
+++ Chaining/KeyProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaining
+{
+    public class KeyProvider
+    {
+        private int counter;
+
+        public int Key()
+        {
+            var key = counter;
+            counter += 1;
+            return key;
+        }
+    }
+}
