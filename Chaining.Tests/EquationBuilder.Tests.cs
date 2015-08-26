9dcca21d0cbacf35d7cb239c146aab248d889d13
cristiingineru <cristi.ingineru@gmail.com>
2015-08-26 22:19:34 +0300

    EquationEditor works with factories

--- Chaining.Tests/EquationBuilder.Tests.cs
+++ Chaining.Tests/EquationBuilder.Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Chaining.Tests
 {
@@ -8,14 +9,134 @@ namespace Chaining.Tests
     {
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void EquationBuilderWithNoFactory_CreateItem_Fails()
+        public void CreateItem_WithNoFactory_Fails()
         {
+            var id = new TestIdentifier();
+
             IEquationBuilder builder = new EquationBuilder();
 
-            builder.CreateItem(new TestIdentifier());
+            builder.CreateItem(id);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateItemReturningItem_WithNoFactory_Fails()
+        {
+            var id = new TestIdentifier();
+
+            IEquationBuilder builder = new EquationBuilder();
+
+            IItem item;
+            builder.CreateItem(id, out item);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateItem_WithFactoryAndUnknownIdentifier_Fails()
+        {
+            var id = new TestIdentifier();
+            var mock = new Mock<IFactory>();
+            mock.Setup(o => o.CanCreate(id)).Returns(false);
+            IEquationBuilder builder = new EquationBuilder(new[] { mock.Object });
+
+            builder.CreateItem(id);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateItemReturningItem_WithFactoryAndUnknownIdentifier_Fails()
+        {
+            var id = new TestIdentifier();
+            var mock = new Mock<IFactory>();
+            mock.Setup(o => o.CanCreate(id)).Returns(false);
+            IEquationBuilder builder = new EquationBuilder(new[] { mock.Object });
+
+            IItem item;
+            builder.CreateItem(id, out item);
+        }
+
+        [TestMethod]
+        public void CreateItemReturningItem_WithFactoryAndKnownIdentifier_ReturnsTheNewItem()
+        {
+            var id = new TestIdentifier();
+            var item = new TestItem();
+            var mock = new Mock<IFactory>();
+            mock.Setup(o => o.CanCreate(id)).Returns(true);
+            mock.Setup(o => o.Create(id)).Returns(item);
+            IEquationBuilder builder = new EquationBuilder(new[] { mock.Object });
+
+            IItem createItem;
+            builder.CreateItem(id, out createItem);
+
+            Assert.AreEqual(item, createItem);
+        }
+
+        [TestMethod]
+        public void CreateItem_WithFactoryAndKnownIdentifier_ReturnsTheBuilder()
+        {
+            var id = new TestIdentifier();
+            var item = new TestItem();
+            var mock = new Mock<IFactory>();
+            mock.Setup(o => o.CanCreate(id)).Returns(true);
+            mock.Setup(o => o.Create(id)).Returns(item);
+            IEquationBuilder builder = new EquationBuilder(new[] { mock.Object });
+
+            var returnedBuilder = builder.CreateItem(id);
+
+            Assert.AreEqual(builder, returnedBuilder);
+        }
+
+        [TestMethod]
+        public void CreateItemReturningItem_WithFactoryAndKnownIdentifier_ReturnsTheBuilder()
+        {
+            var id = new TestIdentifier();
+            var item = new TestItem();
+            var mock = new Mock<IFactory>();
+            mock.Setup(o => o.CanCreate(id)).Returns(true);
+            mock.Setup(o => o.Create(id)).Returns(item);
+            IEquationBuilder builder = new EquationBuilder(new[] { mock.Object });
+
+            IItem createItem;
+            var returnedBuilder = builder.CreateItem(id, out createItem);
+
+            Assert.AreEqual(builder, returnedBuilder);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateItem_WithFactoriesThatCanCreateSameItem_Fails()
+        {
+            var id = new TestIdentifier();
+            var item = new TestItem();
+            var mock = new Mock<IFactory>();
+            mock.Setup(o => o.CanCreate(id)).Returns(true);
+            mock.Setup(o => o.Create(id)).Returns(item);
+            IEquationBuilder builder = new EquationBuilder(new[] { mock.Object, mock.Object });
+
+            builder.CreateItem(id);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateItemReturningItem_WithFactoriesThatCanCreateSameItem_Fails()
+        {
+            var id = new TestIdentifier();
+            var item = new TestItem();
+            var mock = new Mock<IFactory>();
+            mock.Setup(o => o.CanCreate(id)).Returns(true);
+            mock.Setup(o => o.Create(id)).Returns(item);
+            IEquationBuilder builder = new EquationBuilder(new[] { mock.Object, mock.Object });
+
+            IItem createItem;
+            builder.CreateItem(id, out createItem);
         }
 
         private class TestIdentifier : IIdentifier
+        {
+
+        }
+
+        private class TestItem : IItem
         {
 
         }
--- Chaining/EquationBuilder.cs
+++ Chaining/EquationBuilder.cs
@@ -8,19 +8,39 @@ namespace Chaining
 {
     public class EquationBuilder : IEquationBuilder
     {
+        private IEnumerable<IFactory> Factories { get; set; }
+
         public EquationBuilder()
+            : this (Enumerable.Empty<IFactory>())
         {
 
         }
 
         public EquationBuilder(IEnumerable<IFactory> factories)
         {
-
+            Factories = factories;
         }
 
         public EquationBuilder CreateItem(IIdentifier identifier)
         {
-            throw new ArgumentException("unknown identifier");
+            IItem item;
+            return CreateItem(identifier, out item);
+        }
+        public EquationBuilder CreateItem(IIdentifier identifier, out IItem item)
+        {
+            var selectedFactories = Factories.Where(factory => factory.CanCreate(identifier));
+            
+            switch(selectedFactories.Count())
+            {
+                case 0:
+                    throw new ArgumentException("no factory available");
+                case 1: item = selectedFactories.First().Create(identifier);
+                    break;
+                default:
+                    throw new ArgumentException("more than one factory can create a such item");
+            }
+
+            return this;
         }
 
         public Tree<string> ToImmutableTree()
