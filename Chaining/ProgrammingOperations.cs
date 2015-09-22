7f515d72cad5569c1117ceae5463d1bb2b7c5a8b
cristiingineru <cristi.ingineru@gmail.com>
2015-09-22 21:22:10 +0300

    Static/dynamic contract

---
+++ Chaining/ProgrammingOperations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaining
+{
+    public static class ProgrammingOperations
+    {
+        public static T If<T>(this T builder, bool condition, Action<T> onTrue, Action<T> onFalse = null) where T : IEquationBuilder
+        {
+            return builder;
+        }
+
+        public static T Map<T, U>(this T builder, IEnumerable<U> collection, Action<T, U> action) where T : IEquationBuilder
+        {
+            return builder;
+        }
+
+        public static T Map<T, U>(this T builder, IEnumerable<U> collection, Action<T, U, int> action) where T : IEquationBuilder
+        {
+            return builder;
+        }
+
+        public static T Reduce<T, U, V>(this T builder, IEnumerable<U> collection, V seed, Func<U, V, V> action, Action<T, V> onEnd) where T : IEquationBuilder
+        {
+            return builder;
+        }
+
+        public static T Reduce<T, U, V>(this T builder, IEnumerable<U> collection, V seed, Func<U, V, V, int> action, Action<T, V> onEnd) where T : IEquationBuilder
+        {
+            return builder;
+        }
+    }
+}
--- Chaining/Scripting.cs
+++ Chaining/Scripting.cs
@@ -21,11 +21,24 @@ namespace Chaining
     }
 
 
+    /// <summary>
+    /// This is the static contract for each EquationBuilder implementation. We are relying on the runtime contract only
+    /// to allow channing without losing the initial type.
+    /// 
+    /// Any method specified here has to be stronly typed thus all the implementers would be forced to returned the same type.
+    /// </summary>
     public interface IEquationBuilder
     {
 
     }
 
+    /// <summary>
+    /// This is a dynamic contract. Each class that implements the IEquationBuilder has to implement all
+    /// the methods specified here otherwise a client would get a run time exception when using a such not implemented method.
+    /// 
+    /// The functions are generic for returning the exact passed type so channing multiple operations will not
+    /// lose the initial type.
+    /// </summary>
     public static class IEquationBuilderOperations
     {
         public static T Value<T>(this T builder, int constant) where T : IEquationBuilder
@@ -66,35 +79,6 @@ namespace Chaining
             where T : IEquationBuilder
         {
             return (builder as dynamic).CreateItem(identifier, out item);
-        }
-    }
-
-
-    public static class ProgrammingOperations
-    {
-        public static T If<T>(this T builder, bool condition, Action<T> onTrue, Action<T> onFalse = null) where T : IEquationBuilder
-        {
-            return builder;
-        }
-
-        public static T Map<T, U>(this T builder, IEnumerable<U> collection, Action<T, U> action) where T : IEquationBuilder
-        {
-            return builder;
-        }
-
-        public static T Map<T, U>(this T builder, IEnumerable<U> collection, Action<T, U, int> action) where T : IEquationBuilder
-        {
-            return builder;
-        }
-
-        public static T Reduce<T, U, V>(this T builder, IEnumerable<U> collection, V seed, Func<U, V, V> action, Action<T, V> onEnd) where T : IEquationBuilder
-        {
-            return builder;
-        }
-
-        public static T Reduce<T, U, V>(this T builder, IEnumerable<U> collection, V seed, Func<U, V, V, int> action, Action<T, V> onEnd) where T : IEquationBuilder
-        {
-            return builder;
         }
     }
 }
